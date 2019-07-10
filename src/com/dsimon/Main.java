092130c65facfb8d6aab17639a5f816cd8506ec9
Hurlyburly2 <dougdougmann@gmail.com>
2019-07-09 23:52:09 -0400

    challenge complete

--- src/com/dsimon/Main.java
+++ src/com/dsimon/Main.java
@@ -1,8 +1,33 @@
 package com.dsimon;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int[] array = {1, 2, 3, 4, 5};
+
+        System.out.println(Arrays.toString(array));
+	    reverse(array);
+        System.out.println(Arrays.toString(array));
+
+        int[] secondArray = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(secondArray));
+        reverse(secondArray);
+        System.out.println(Arrays.toString(secondArray));
+
+    }
+
+    private static void reverse(int[] array) {
+        int firstCounter = 0;
+        int lastCounter = array.length - 1;
+
+        while (firstCounter < lastCounter) {
+            int tempNumber = array[firstCounter];
+            array[firstCounter] = array[lastCounter];
+            array[lastCounter] = tempNumber;
+            firstCounter++;
+            lastCounter--;
+        }
     }
 }
